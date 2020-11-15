3cc8d29ca1f2b993226474adbcaeefa1c54c7280
Mauro Mastrapasqua <mauro.mastrapasqua@studenti.unimi.it>
2020-11-15 18:10:58 +0100

    Aggiunto file per i3-gaps

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,5 +1,5 @@
 sudo pacman -S --needed \
 	make automake autoconf pkg-config gcc fakeroot binutils \
-	pavucontrol mpd wmctrl rofi polybar
+	pavucontrol mpd wmctrl rofi polybar feh
 
 yay -S siji-git nerd-fonts-jetbrains-mono
