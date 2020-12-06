1c2b482f4e3bd356fc35aeb95a7bbf4ca3e53d2f
Mauro Mastrapasqua <mauro.mastrapasqua@studenti.unimi.it>
2020-12-06 22:43:08 +0100

    Final

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
-rm -rf ~/.config/i3/* ~/.config/polybar/* ~/.config/rofi/*
+rm -rf ~/.config/i3/* ~/.config/polybar/* ~/.config/rofi/* ~/.config/picom/*
 cp -r i3/* ~/.config/i3/
 cp -r polybar/* ~/.config/polybar/
 cp -r rofi/* ~/.config/rofi/
+cp -r picom/* ~/.config/picom/
